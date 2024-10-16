0368c089617214501aca391aac8c7fbaadcb387c
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-10-16 11:17:33 +0200

    debugging

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -28,7 +28,7 @@ server.post("/api/message", (req: Request, res: Response) => {
         console.log(`the error is ${error}`);
       } else {
         console.log("message sent is" + info.response);
-        res.send("message sent");
+        res.json({ message: "message sent" });
       }
     }
   );
